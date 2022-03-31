24fd6e783414606c3a821bd464ab0c7a75139b98
imakathuria <imakathuria@gmail.com>
2022-03-31 15:32:43 +0530

    Data Abstraction and Abstract classes

---
+++ March-31-2022/code 7/Abstraction.java
@@ -0,0 +1,28 @@
+ abstract class LivingBeing{
+	 abstract void talk();
+ }
+ 
+ abstract class Person extends LivingBeing{
+	String fname,lname;
+	
+	void talk(){
+		System.out.println(this.fname+" is talking");
+	}
+	void walk(){
+		System.out.println(this.fname+" is walking");
+	}
+	void eat(){
+		System.out.println(this.fname+" is eating");
+	}
+	//String args
+	public static void main(String args[]){
+		Person p1=new Person();
+		p1.fname="Sachin";
+		p1.lname="Tendulkar";
+		p1.talk();
+		p1.walk();
+		p1.eat();
+	}
+	
+}
+
